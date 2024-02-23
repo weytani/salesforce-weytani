5aaf4efeb1bb16c1cd92bc98511517775dce93a4
weytani <david@weytani.com>
2024-02-22 21:04:02 -0700

    more things

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class Home extends LightningElement {}
+export default class Home extends LightningElement {
+  @track isScrolled = false;
+
+  renderedCallback() {
+    this.setUpScrollListener();
+  }
+
+  setUpScrollListener() {
+    const callback = () => {
+      this.isScrolled = window.scrollY > 0;
+    };
+
+    window.addEventListener("scroll", callback);
+
+    this.addEventListener("destroy", () => {
+      window.removeEventListener("scroll", callback);
+    });
+  }
+
+  get headerClasses() {
+    return `header ${this.isScrolled ? "header-scrolled" : ""}`;
+  }
+}
